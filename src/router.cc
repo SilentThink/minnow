00a99c8c4db7b1eec0fd3ea259582f5b6736384a
Your Name <your_email@example.com>
2024-10-20 00:09:48 +0800

    check6 not pass yet

--- src/router.cc
+++ src/router.cc
@@ -1,3 +1,4 @@
+// router.cc
 #include "router.hh"
 
 #include <iostream>
@@ -21,10 +22,61 @@ void Router::add_route( const uint32_t route_prefix,
        << " on interface " << interface_num << "\n";
 
   // Your code here.
+  _route_table[route_prefix] = RouteTableEntry(prefix_length, next_hop, interface_num);
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
+  // The “match”:The Router searches the routing table to find the routes that match the datagram’s
+  // destination address. By “match,” we mean the most-significant prefix length bits of
+  // the destination address are identical to the most-significant prefix length bits of the
+  // route prefix.
+
+  // The “action”: what to do if the route matches and is chosen. If the router is
+  // directly attached to the network in question, the next hop will be an empty optional.
+  // In that case, the next hop is the datagram’s destination address. But if the router is
+  // connected to the network in question through some other router, the next hop will
+  // contain the IP address of the next router along the path. The interface num gives the
+  // index of the router’s NetworkInterface that should use to send the datagram to the
+  // next hop. You can access this interface with the interface(interface num) method.
+  
   // Your code here.
+  size_t interface_size = _interfaces.size();
+
+  for (size_t in_num = 0; in_num < interface_size; in_num++) {
+
+    std::queue<InternetDatagram> datagrams = interface(in_num).get()->datagrams_received();
+
+    while (!datagrams.empty()) {
+      auto datagram = datagrams.front();
+      // Find the route that matches the datagram's destination address
+      uint32_t dst =  datagram.header.dst;
+      uint8_t ttl = (datagram.header.ttl--);
+      if (ttl <= 0) {
+        datagrams.pop();
+        continue;
+      }
+      for (auto route : _route_table) {
+        const uint8_t prefix_length = route.second.prefix_length;
+        const uint32_t route_prefix = route.first;
+        if ((dst & ~((1 << (32 - prefix_length)) - 1)) == (route_prefix)) {
+          // The datagram's destination address matches the route prefix
+          // Send the datagram out the corresponding interface
+          size_t out_num = route.second.interface_num;
+          if (route.second.next_hop.has_value()) {
+            // Send the new datagram out the corresponding interface
+            interface(out_num)->send_datagram(datagram,*route.second.next_hop);
+          } else if(!route.second.next_hop.has_value()) {
+            Address dst_address = Address::from_ipv4_numeric(datagram.header.dst);
+            interface(out_num)->send_datagram(datagram,dst_address);
+          }
+          break;
+        }
+      }
+
+      datagrams.pop();
+    }
+
+  }
 }
--- src/router.hh
+++ src/router.hh
@@ -1,3 +1,4 @@
+// router.hh
 #pragma once
 
 #include <memory>
@@ -5,6 +6,25 @@
 
 #include "exception.hh"
 #include "network_interface.hh"
+
+struct RouteTableEntry
+{
+  // uint32_t route_prefix;
+  uint8_t prefix_length;
+  std::optional<Address> next_hop;
+  size_t interface_num;
+
+  // 默认构造函数
+  RouteTableEntry() : prefix_length(0), next_hop(std::nullopt), interface_num(0) {}
+
+  RouteTableEntry(uint8_t pl, std::optional<Address> nh, size_t in)
+      : prefix_length(pl), next_hop(nh), interface_num(in) {}
+
+  bool operator<( const RouteTableEntry& other ) const
+  {
+    return prefix_length > other.prefix_length;
+  }
+};
 
 // \brief A router that has multiple network interfaces and
 // performs longest-prefix-match routing between them.
@@ -35,4 +55,6 @@ public:
 private:
   // The router's collection of network interfaces
   std::vector<std::shared_ptr<NetworkInterface>> _interfaces {};
+
+  std::map<uint32_t, RouteTableEntry> _route_table {};
 };
